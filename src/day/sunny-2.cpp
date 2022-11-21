c8b7d996c329c83b00b67e4ba97b4f71795a742c
huxiaofeng3 <yfhuxiaofeng@jd.com>
2022-11-21 03:05:24 +0000

    2nd

---
+++ src/day/sunny-2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sunny_age=10;
+    int dad_age=42;
+
+    //求差
+    int diff=dad_age-sunny_age;
+    cout<<diff<<endl;
+
+    //求和
+    int sum =sunny_age+dad_age;
+    cout<<sum<<endl;
+    /**
+     * 如果 if
+     * 少年 按0~14岁
+     * 壮年 15~64岁
+     * 老年 65岁以上
+    */
+
+   int age;
+   cin>>age;
+   if (age<15)
+   {
+    cout<<"you are a kid."<<endl;
+   }
+   if (age<65 && age>14)
+   {
+    cout<<"you are a young."<<endl;
+   }
+    if (age>64)
+   {
+    cout<<"you are a old."<<endl;
+   }
+
+    return 0;
+}
