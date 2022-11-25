c37f8cde5263515c60fc9e547ca9f00152564b96
huxiaofeng3 <yfhuxiaofeng@jd.com>
2022-11-25 13:09:16 +0000

    乘法

--- src/day/sunny-3.cpp
+++ src/day/sunny-3.cpp
@@ -10,13 +10,21 @@ int main()
         sum = sum + i;
     }
     cout << "sum=" << sum << endl;
+
+     // 1-100的和
     int he = 0;
     for (int b = 1; b <= 100; b++)
     {
         he = he + b;
     }
     cout << "he=" << he << endl;
-    // 1-100的和
-
+   
+   //从1乘到10
+ int ji = 1;
+    for (int c = 1; c <= 10; c++)
+    {
+        ji = ji *c;
+    }
+     cout << "ji=" << ji << endl;   
     return 0;
 }
